48ef170ae938091fa579760e2b7523d4dcade8c5
siaikin <abc1310054026@outlook.com>
2025-08-10 16:52:14 +0800

    fix: allow data to be null or undefined in gueleton

--- src/client/vue/gueleton.ts
+++ src/client/vue/gueleton.ts
@@ -20,7 +20,7 @@ export type GueletonProps<DATA> = {
   /**
    * 代理的原始数据. gueleton 将根据原始数据和 limit 参数生成预存数据.
    */
-  data: DATA
+  data: DATA | null | undefined
   /**
    * 这个参数可以让你直接在代码中设置预存数据, 而不依赖构建时的 vite/webpack 插件.
    * 当 prestoreData 不为空时, 会直接使用 prestoreData, 而不会根据 data 生成预存数据.
