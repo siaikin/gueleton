80f780b9b2da2ff48f6b27f3c4e23dd4d19c3598
siaikin <abc1310054026@outlook.com>
2025-08-28 13:40:15 +0800

    feat: 更新 unplugin-gueleton 版本至 0.1.43，优化 useGueleton 函数以支持空容器处理

--- packages/unplugin-gueleton/src/client/vue/use-gueleton.ts
+++ packages/unplugin-gueleton/src/client/vue/use-gueleton.ts
@@ -1,9 +1,10 @@
 import type { ComponentPublicInstance, ComputedRef, CSSProperties, MaybeRefOrGetter } from 'vue'
 import type { GueletonOptions } from '../core'
 import type { ToMaybeRefOrGetter } from './utils'
-import { computed, shallowReactive, toValue, watch } from 'vue'
+import { readonly, ref, shallowReactive, toValue, watch } from 'vue'
 import { Gueleton } from '../core'
 import { refOrGetterMapToRaw } from './utils'
+import { isNil } from 'lodash-es'
 
 export function useGueleton<T>(
   dataKey: MaybeRefOrGetter<GueletonOptions<T, CSSProperties>['dataKey']>,
@@ -13,8 +14,17 @@ export function useGueleton<T>(
   shouldRender: ComputedRef<boolean>
   renderData: ComputedRef<GueletonOptions<T, CSSProperties>['data']>
 } {
+  const shouldRender = ref(false)
+  const renderData = ref<GueletonOptions<T, CSSProperties>['data']>(undefined)
+
   let gueleton: Gueleton<T, CSSProperties>
-  watch(() => toValue(dataKey), _dataKey => gueleton = shallowReactive(new Gueleton(_dataKey, refOrGetterMapToRaw(options))), { immediate: true })
+  watch(() => toValue(dataKey), (_dataKey) => {
+    gueleton = shallowReactive(new Gueleton(_dataKey, refOrGetterMapToRaw(options)))
+    gueleton.onOptionsUpdate = () => {
+      shouldRender.value = gueleton.shouldRender
+      renderData.value = gueleton.renderData
+    }
+  }, { immediate: true })
 
   const propRefs = Object.values(options)
 
@@ -23,13 +33,18 @@ export function useGueleton<T>(
       refOrGetterMapToRaw(options),
       () => {
         const _container = toValue(container)
+
+        if (isNil(_container)) {
+          return null
+        }
+
         return _container instanceof Element ? _container : _container?.$el
       },
     )
   }, { immediate: true })
 
   return {
-    shouldRender: computed(() => gueleton.shouldRender),
-    renderData: computed(() => gueleton.renderData),
+    shouldRender: readonly(shouldRender),
+    renderData: readonly(renderData),
   }
 }
