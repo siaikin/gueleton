dd7aef5d2ff4d4c55e05aec2a1cf88f556328286
siaikin <abc1310054026@outlook.com>
2025-08-29 23:22:22 +0800

    fix: 修复样式处理逻辑，确保正确处理骨架样式和类名

--- packages/unplugin-gueleton/src/client/core/setup-skeleton-bone.ts
+++ packages/unplugin-gueleton/src/client/core/setup-skeleton-bone.ts
@@ -12,7 +12,7 @@ export function setupSkeletonBone<CSSTYPE>(bone: HTMLElement, source: HTMLElemen
   const styleMap = source.computedStyleMap()
 
   for (const key of Object.keys(options.bone.style || {})) {
-    const value = options.bone.style[key as keyof CSSTYPE]
+    const value = options.bone.style?.[key as keyof CSSTYPE]
     if (value) {
       bone.style.setProperty(kebabCase(key), value.toString())
     }
@@ -23,7 +23,7 @@ export function setupSkeletonBone<CSSTYPE>(bone: HTMLElement, source: HTMLElemen
   }
 
   const className = isArray(options.bone.className) ? options.bone.className.join(' ') : options.bone.className
-  className.length > 0 && bone.classList.add(...className.split(' '))
+  className && bone.classList.add(...className.split(' '))
 
   // boneNode.dataset.gueletonBoneSkip = 'true'
 }
@@ -36,23 +36,24 @@ export function createSkeletonContainer<CSSTYPE>(source: HTMLElement, _options: 
 }
 
 export function setupSkeletonContainer<CSSTYPE>(container: HTMLElement, options: SkeletonOptions<CSSTYPE>): void {
+  container.style.setProperty('display', 'block')
+  container.style.setProperty('position', 'absolute')
+  container.style.setProperty('top', '0')
+  container.style.setProperty('left', '0')
+  container.style.setProperty('width', '100%')
+  container.style.setProperty('height', '100%')
+  container.style.setProperty('margin', '0')
+  container.style.setProperty('z-index', '10')
+
   for (const key of Object.keys(options.container.style || {})) {
-    const value = options.container.style[key as keyof CSSTYPE]
+    const value = options.container.style?.[key as keyof CSSTYPE]
     if (value) {
       container.style.setProperty(kebabCase(key), value.toString())
     }
   }
-  container.style.setProperty('display', 'block', 'important')
-  container.style.setProperty('position', 'absolute', 'important')
-  container.style.setProperty('top', '0', 'important')
-  container.style.setProperty('left', '0', 'important')
-  container.style.setProperty('width', '100%', 'important')
-  container.style.setProperty('height', '100%', 'important')
-  container.style.setProperty('margin', '0', 'important')
-  container.style.setProperty('z-index', '10', 'important')
 
   const className = isArray(options.container.className) ? options.container.className.join(' ') : options.container.className
-  className.length > 0 && container.classList.add(...className.split(' '))
+  className && container.classList.add(...className.split(' '))
 
   // container.dataset.gueletonBoneSkip = 'true'
 }
